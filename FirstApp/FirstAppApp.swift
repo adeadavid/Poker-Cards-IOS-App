0f1b109b1c28c3774c2e23f959b09bad87e11dfe
adeadavid <adeadavid@gmail.com>
2022-04-23 15:18:27 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Afolabi Adegorite on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
